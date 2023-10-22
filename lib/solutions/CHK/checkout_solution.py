9d930029d114c2e4e349cab16e35ea6fc6bce4d1
sbx_user1051 <sbx_user1051@169.254.69.237>
2023-10-22 14:32:50 +0000

    Sun Oct 22 14:32:50 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -29,8 +29,8 @@ specials = {
 
 def apply_discounts(basket):
     for code, count in basket.items():
-        if code in specials:
-            if count
+        if code in specials and count >= specials[code][0]:
+            print(count // specials[code][0])
 
 def checkout(skus):
     basket = {}
@@ -39,10 +39,12 @@ def checkout(skus):
         basket[item] = basket.get(item, 0) + 1
 
     # We have a count of the items, we need some way to define the special offers to apply the discord
-
+    apply_discounts(basket)
 
     return basket
 
 print(checkout(["A", "A", "A", "B", "D"]))
+print(checkout(["A", "A", "A", "A", "A", "A", "A", "B", "D"]))
+
 
 
