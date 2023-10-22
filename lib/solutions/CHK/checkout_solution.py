21250e5b9cd0570c74bbdcbb0c9ec9b0d54c79ab
sbx_user1051 <sbx_user1051@169.254.69.237>
2023-10-22 14:29:50 +0000

    Sun Oct 22 14:29:50 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,11 +2,47 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+"""
+Our price table and offers:
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+"""
+
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
+specials = {
+    "A": (3, 130),
+    "B": (2, 45),
+}
+
+def apply_discounts(basket):
+    for code, count in basket.items():
+        if code in specials:
+            if count
+
 def checkout(skus):
     basket = {}
+    # Remap to count
     for item in skus:
         basket[item] = basket.get(item, 0) + 1
+
+    # We have a count of the items, we need some way to define the special offers to apply the discord
+
+
     return basket
 
 print(checkout(["A", "A", "A", "B", "D"]))
+
 
