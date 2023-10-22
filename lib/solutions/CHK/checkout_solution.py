653b83f362fac172ff6194f28411b5d5697a4733
sbx_user1051 <sbx_user1051@169.254.69.237>
2023-10-22 14:38:50 +0000

    Sun Oct 22 14:38:50 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -27,10 +27,24 @@ specials = {
     "B": (2, 45),
 }
 
+
 def apply_discounts(basket):
+    """
+    Return a reduced count of items in the basket, and the cost of what offers were applied
+    :param basket:
+    :return:
+    """
+    cost_of_discounted = []
     for code, count in basket.items():
-        if code in specials and count >= specials[code][0]:
-            print(count // specials[code][0])
+        if code not in specials:
+            continue
+        required_items = specials[code][0]
+        if count >= required_items:
+            fits = count // required_items
+            basket[code] = basket[code] - fits*required_items
+            cost_of_discounted.append(fits*specials[code][1])
+    return basket, cost_of_discounted
+
 
 def checkout(skus):
     basket = {}
@@ -38,13 +52,15 @@ def checkout(skus):
     for item in skus:
         basket[item] = basket.get(item, 0) + 1
 
+    print(basket)
     # We have a count of the items, we need some way to define the special offers to apply the discord
-    apply_discounts(basket)
+    new_basket, discount_costs = apply_discounts(basket)
+    print(new_basket, discount_costs)
 
-    return basket
 
-print(checkout(["A", "A", "A", "B", "D"]))
+print(checkout(["A", "A", "A", "B", "D", "B"]))
 print(checkout(["A", "A", "A", "A", "A", "A", "A", "B", "D"]))
+
 
 
 
